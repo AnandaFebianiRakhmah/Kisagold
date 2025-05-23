d46308aa4e48d35ceedb652aa6e186a4da76a992
AnandaFebianiRakhmah <anandafebianirakhmah@gmail.com>
2025-05-23 09:39:26 +0700

    Update .gitignore

--- kisagold/settings.py
+++ kisagold/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0brw*$3lo-!q_=*9l-&##)=3x!v(#$y_ex&=s8l-^zw!6%fmk)"
+# SECRET_KEY = "django-insecure-0brw*$3lo-!q_=*9l-&##)=3x!v(#$y_ex&=s8l-^zw!6%fmk)"
+SECRET_KEY = os.environ['django-insecure-0brw*$3lo-!q_=*9l-&##)=3x!v(#$y_ex&=s8l-^zw!6%fmk)']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
